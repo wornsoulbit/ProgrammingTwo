aaf837cb39181077faa750168d5024e470943533
Alex Vasil <alexandervasil840@gmail.com>
2020-03-19 18:44:20 -0400

    Added JavaDocs to all the methods that was previously missing them.

--- Assignments/A2/Assignment2B/src/assignment2b/CharArrayTools.java
+++ Assignments/A2/Assignment2B/src/assignment2b/CharArrayTools.java
@@ -4,36 +4,73 @@ package assignment2b;
 import java.util.ArrayList;
 
 /**
+ * Char array tools done recursively.
  * 
  * @author Alex Vasil
  */
 public class CharArrayTools {
     
+    /**
+     * Checks how many of any given letter is in a char ArrayList.
+     * 
+     * @param list ArrayList with each character.
+     * @param ch The given letter.
+     * @return How many occurrences of the given letter is in the arrayList.
+     */
     public static int howMany(ArrayList<Character> list, char ch) {
         return howMany(list, ch, list.size() - 1, 0);
     }
     
-    private static int howMany(ArrayList<Character> list, char ch, int n, int occurances) {
+    /**
+     * Checks how many of any given letter is in a char ArrayList recursively.
+     * 
+     * @param list ArrayList with each character.
+     * @param ch The given letter.
+     * @param n Start point.
+     * @param occurrences The number of occurrences of the given letter.
+     * @return The number of occurrences of the given letter.
+     */
+    private static int howMany(ArrayList<Character> list, char ch, int n, int occurrences) {
         if (n == -1)
-            return occurances;
+            return occurrences;
         else 
-            return list.get((char) n) == ch ? howMany(list, ch, n - 1, occurances + 1) 
-                    : howMany(list, ch, n - 1, occurances);
+            return list.get((char) n) == ch ? howMany(list, ch, n - 1, occurrences + 1) 
+                    : howMany(list, ch, n - 1, occurrences);
     }
     
+    /**
+     * Checks how many of a given character occurs in a given string.
+     * 
+     * @param str The given string.
+     * @param ch The given character.
+     * @return The number of occurrences that character occurred in the given string.
+     */
     public static int howMany(String str, char ch) {
         return howMany(str, ch, str.length() - 1, 0);
     }
-
     
-    private static int howMany(String str, char ch, int n, int occurances) {
+    /**
+     * Checks how many of a given character occurs in a given string recursively.
+     * 
+     * @param str The given string.
+     * @param ch The given character.
+     * @param n Start point.
+     * @param occurrences The number of occurrences the given character occurred in the given string.
+     * @return The number of occurrences of the given character in the string.
+     */
+    private static int howMany(String str, char ch, int n, int occurrences) {
         if (n == -1)
-            return occurances;
+            return occurrences;
         else 
-            return str.charAt(n) == ch ? howMany(str, ch, n - 1, occurances + 1) 
-                    : howMany(str, ch, n - 1, occurances);
+            return str.charAt(n) == ch ? howMany(str, ch, n - 1, occurrences + 1) 
+                    : howMany(str, ch, n - 1, occurrences);
     }
     
+    /**
+     * 
+     * @param list
+     * @return 
+     */
     public static boolean isPalindrome(ArrayList<Character> list) {
         return isPalindrome(list, list.size() - 1);
     }
--- Assignments/A2/Assignment2B/src/assignment2b/RecursiveMultiplication.java
+++ Assignments/A2/Assignment2B/src/assignment2b/RecursiveMultiplication.java
@@ -7,6 +7,13 @@ package assignment2b;
  * @author Alex Vasil
  */
 public class RecursiveMultiplication {
+    /**
+     * Recursively multiplies any given two numbers with x multiplied by y.
+     * 
+     * @param x The first integer.
+     * @param y The second integer.
+     * @return The multiplied number of x and y.
+     */
     public static int multiply(int x, int y) {
         if (x == 0 || y == 0)
             return 0;
